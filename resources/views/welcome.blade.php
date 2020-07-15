c3c5ce780ab17de232cc3eeafec4f94f444839f5
Brayan <bguillenn11@gmail.com>
2020-07-14 21:31:34 -0500

    Landing page primera paginañ

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,101 +1,34 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-
-        <title>Laravel</title>
-
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
-
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 42px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ComiGeo: Sistema de geolocalización de comisarias</title>
+    <link rel="stylesheet" href="{{ asset('css/landing.css') }}"/>
+</head>
+<body>
+    <div id="header">
+        <img src="https://res.cloudinary.com/dtg90uzhc/image/upload/v1594699993/comigeo/ugvpuhf3xu8o3rvgaeii.svg" alt="logo-comigeo" width="210px" heigth="45px">
+        <button id="header-button" class="button-landing">IR AL MAPA</button>
+    </div>
+    <div id="content">
+        <div id="page-1">
+            <div id="info-text">
+                <div>
+                    <div id="rectangle"></div>
+                    <p>
+                        Ubica geograficamente comisarias a nivel nacional y encuentra la mas cercana a tu ubicación. 
+                    </p>
                 </div>
-            @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                    <p align="center"><img src="https://res.cloudinary.com/dtg90uzhc/image/upload/v1593486684/comigeo/t4y7shwt6ds5fnlyaa8d.png" width="600"></p>
-                    Localiza comisarias peruanas
-                </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://vapor.laravel.com">Vapor</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
+                <button class="button-landing">
+                    IR AL MAPA
+                </button>
             </div>
+            <img src="https://res.cloudinary.com/dtg90uzhc/image/upload/v1594699979/comigeo/qkpdnihzk5th7pigeeyi.svg" alt="person-map"width="534px" heigth="535px">
         </div>
-    </body>
+        <div id="page22">
+            <p>Denuncias familiares aqui</p>
+        </div>
+    </div>
+</body>
 </html>
