c498c1151226c0825b32e4729dc8c710da82a2b0
Brayan <bguillenn11@gmail.com>
2020-07-15 00:51:37 -0500

    Avances en el landing

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,9 +7,9 @@
     <link rel="stylesheet" href="{{ asset('css/landing.css') }}"/>
 </head>
 <body>
-    <div id="header">
+    <div id="header" class="">
         <img src="https://res.cloudinary.com/dtg90uzhc/image/upload/v1594699993/comigeo/ugvpuhf3xu8o3rvgaeii.svg" alt="logo-comigeo" width="210px" heigth="45px">
-        <button id="header-button" class="button-landing">IR AL MAPA</button>
+        <button class="button-landing header-button-hide">IR AL MAPA</button>
     </div>
     <div id="content">
         <div id="page-1">
@@ -26,8 +26,65 @@
             </div>
             <img src="https://res.cloudinary.com/dtg90uzhc/image/upload/v1594699979/comigeo/qkpdnihzk5th7pigeeyi.svg" alt="person-map"width="534px" heigth="535px">
         </div>
-        <div id="page22">
-            <p>Denuncias familiares aqui</p>
+        <div id="page-2">
+            <h1>Denuncias por violencia familiar</h1>
+            <p>En esta grafica se muestra la cantidad de denuncias por violencia familiar puesta cada mes por hombres y mujeres</p>
+            <div>
+                <div id="char"></div>
+                <div id="data">
+                    <h2>CIFRAS</h2>
+                    <h6>Total de denuncias</h6>
+                    <h1>250 000</h1>
+                    <h6>Puestas por hombres</h6>
+                    <h1>125 000</h1>
+                    <h6>Puestas por mujeres</h6>
+                    <h1>125 000</h1>
+                </div> 
+            </div>
+        </div>
+        <div id="page-3">
+            <h1>&lt;Equipo de Desarrollo&gt;</h1>
+            <div class="team">
+                <div class="miembro-card">
+                    <img src="" alt="" class="miembro-photo">
+                    <p class="miembro-rol">Lider</p>
+                    <h2>BRAYAN F. GUILLEN</h2>
+                    <p>bguillenn@unsa.edu.pe</p>
+                </div>
+                <div class="miembro-card">
+                    <img src="" alt="" class="miembro-photo">
+                    <p class="miembro-rol">Pruebas</p>
+                    <h2>JHONATAN J. ACUÑA</h2>
+                    <p>jacuñah@unsa.edu.pe</p>
+                </div>
+                <div class="miembro-card">
+                    <img src="" alt="" class="miembro-photo">
+                    <p class="miembro-rol">Lider</p>
+                    <h2>DENIS A. LLAMOCA</h2>
+                    <p>dllamocac@unsa.edu.pe</p>
+                </div>
+            </div>
+            <div class="team">
+                <div class="miembro-card">
+                    <img src="" alt="" class="miembro-photo">
+                    <p class="miembro-rol">Lider</p>
+                    <h2>BRAYAN F. GUILLEN</h2>
+                    <p>bguillenn@unsa.edu.pe</p>
+                </div>
+                <div class="miembro-card">
+                    <img src="" alt="" class="miembro-photo">
+                    <p class="miembro-rol">Lider</p>
+                    <h2>BRAYAN F. GUILLEN</h2>
+                    <p>bguillenn@unsa.edu.pe</p>
+                </div>
+                <div class="miembro-card">
+                    <img src="" alt="" class="miembro-photo">
+                    <p class="miembro-rol">Lider</p>
+                    <h2>BRAYAN F. GUILLEN</h2>
+                    <p>bguillenn@unsa.edu.pe</p>
+                </div>
+            </div>
+            <h1>&lt;/Equipo de Desarrollo&gt;</h1>
         </div>
     </div>
 </body>
