34247dc869d7ab53ba28666eb5db8be8409cab3f
David <amolloa@unsa.edu.pe>
2020-08-07 03:25:00 -0500

    Funcionamiento de las rutas requeridas
    - /comisarias
    - /comisarias/{lat}/{lon}
    - /comisarias/{id} -> al ingresar números
    - /comisarias/{query} -> al ingresar texto (o texto con números)
    - /comisarias/{id}/{lat}/{lon}
    - /departamentos
    - /departamentos/{id}/provincias
    - /provincias/{id}/distritos

--- app/Http/Controllers/ComisariaController.php
+++ app/Http/Controllers/ComisariaController.php
@@ -25,7 +25,24 @@ class ComisariaController extends Controller{
 
 	public function cercano($lat, $lon)
 	{
-		return "Mostrando la comisaria más cercana a la posición $lat,$lon";
+		$rango = 2.2;
+		$comisarias=Comisaria::all();
+		$dist = 2500;
+		$id = 1;
+		foreach ($comisarias as $comisaria) {
+			if(abs($lat-$comisaria->ComLat)<$rango && abs($lon-$comisaria->ComLon)<$rango)
+			{
+				$tmp = $this->distanceCalculation($lat,$lon,$comisaria);
+				if($tmp<$dist)
+				{
+					$dist = $tmp;
+					$id = $comisaria->ComCod;
+				}
+			}
+		}
+		$result = $this->datos(Comisaria::find($id));
+		return response()->json([$id=>$result],200);
+
 	}
 
 	public function show($id)
@@ -33,7 +50,7 @@ class ComisariaController extends Controller{
 		$comisaria=Comisaria::find($id);
 		if(!$comisaria)
 		{
-			return response()->json(['mensaje'=>'No se encuentra la comisaría','codigo'=>404],404);
+			return response()->json(['mensaje'=>'No se encuentra la comisaria','codigo'=>404],404);
 		}
 		$result = $this->datos($comisaria);
 
@@ -42,15 +59,38 @@ class ComisariaController extends Controller{
 
 	public function distancia($id, $lat, $lon)
 	{
-		return "Mostrando la distancia de $lat,$lon hasta $id";
+		$comisaria = Comisaria::find($id);
+		if(!$comisaria)
+		{
+			return response()->json(['mensaje'=>'No se encuentra la comisaria','codigo'=>404],404);
+		}
+		$dist = round($this->distanceCalculation($lat,$lon,$comisaria), 3);
+		return response()->json(['distancia'=>['kms'=>$dist]],200);
 	}
 
 	public function buscar($query)
 	{
-		return "Mostrando las comisarías que cumplen con la búsqueda $query";
+		$query = strtoupper($this->eliminar_acentos($query));
+		$comisarias=Comisaria::all();
+		$result = [];
+		foreach ($comisarias as $comisaria) 
+		{
+			if($this->contieneQuery($comisaria,$query))
+			{
+				$codigo=$comisaria->ComCod;
+				$datos=$this->datos($comisaria);
+				$result[$codigo]=$datos;
+			}
+		}
+		if(!count($result))
+		{
+			return response()->json(['mensaje'=>'No se encuentra coincidencias.','codigo'=>404],404);
+		}
+		return response()->json(['comisarias'=>$result],200);
 	}
 
-	public function importExcel(Request $request){
+	public function importExcel(Request $request)
+	{
 		Comisaria::get()->each->delete();
 		$file = $request->file('file');
 		Excel::import(new ComisariaImport, $file);
@@ -76,7 +116,74 @@ class ComisariaController extends Controller{
 				];
 		return $result;
 	}
+
+	function distanceCalculation($point1_lat, $point1_long, $comisaria) 
+	{
+		// Cálculo de la distancia en grados
+		$point2_lat = $comisaria->ComLat;
+		$point2_long = $comisaria->ComLon;
+		$degrees = rad2deg(acos((sin(deg2rad($point1_lat))*sin(deg2rad($point2_lat))) + (cos(deg2rad($point1_lat))*cos(deg2rad($point2_lat))*cos(deg2rad($point1_long-$point2_long)))));
+
+		$distance = $degrees * 111.13384;
+
+		return $distance;
+	}
+
+	function eliminar_acentos($cadena)
+	{    
+        //Reemplazamos la A y a
+        $cadena = str_replace(
+        array('Á', 'À', 'Â', 'Ä', 'á', 'à', 'ä', 'â', 'ª'),
+        array('A', 'A', 'A', 'A', 'a', 'a', 'a', 'a', 'a'),
+        $cadena
+        );
+ 
+        //Reemplazamos la E y e
+        $cadena = str_replace(
+        array('É', 'È', 'Ê', 'Ë', 'é', 'è', 'ë', 'ê'),
+        array('E', 'E', 'E', 'E', 'e', 'e', 'e', 'e'),
+        $cadena );
+ 
+        //Reemplazamos la I y i
+        $cadena = str_replace(
+        array('Í', 'Ì', 'Ï', 'Î', 'í', 'ì', 'ï', 'î'),
+        array('I', 'I', 'I', 'I', 'i', 'i', 'i', 'i'),
+        $cadena );
+ 
+        //Reemplazamos la O y o
+        $cadena = str_replace(
+        array('Ó', 'Ò', 'Ö', 'Ô', 'ó', 'ò', 'ö', 'ô'),
+        array('O', 'O', 'O', 'O', 'o', 'o', 'o', 'o'),
+        $cadena );
+ 
+        //Reemplazamos la U y u
+        $cadena = str_replace(
+        array('Ú', 'Ù', 'Û', 'Ü', 'ú', 'ù', 'ü', 'û'),
+        array('U', 'U', 'U', 'U', 'u', 'u', 'u', 'u'),
+        $cadena );
+ 
+        //Reemplazamos la N, n, C y c
+        $cadena = str_replace(
+        array('ñ', 'Ç', 'ç', '-'),
+        array('Ñ', 'C', 'c','_'),
+        $cadena
+        );
+        
+        return $cadena;
+    }
 	
+	function contieneQuery($comisaria,$query)
+	{
+		$nombre = $comisaria->ComNom;
+		if(strpos($nombre, $query)!==false)
+		{
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}
 }
 
  ?>
