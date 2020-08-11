5cdf3b4342b7be2624f28493ef6612519f82d8d2
David <amolloa@unsa.edu.pe>
2020-08-11 08:11:09 -0500

    Api de votos completa

--- app/Http/Controllers/VotoComisariaController.php
+++ app/Http/Controllers/VotoComisariaController.php
@@ -41,4 +41,50 @@ class VotoComisariaController extends Controller
 		}
 		return response()->json(['response'=>['realizado'=>true, 'mensaje'=>'']],202);
 	}
+
+	public function voto($id, Request $request)
+	{
+		$comisaria=Comisaria::find($id);
+		if(!$comisaria)
+		{
+			return response()->json(['response'=>['realizado'=>false, 'mensaje'=>'No se encuentra la comisaria']],404);
+		}
+
+		$ip=$request->ip();
+		$puntaje = 0;
+		$voto=VotoComisaria::find($ip);
+		if(!$voto || $voto->ComCod != $id)
+		{
+			return response()->json(['response'=>['ip'=>$ip, 'puntaje'=>0]],200);
+		}
+		else
+		{
+			return response()->json(['response'=>['ip'=>$ip, 'puntaje'=>$voto->VotPun]],200);
+		}
+	}
+
+	public function puntaje($id)
+	{
+		$comisaria=Comisaria::find($id);
+		if(!$comisaria)
+		{
+			return response()->json(['response'=>['realizado'=>false, 'mensaje'=>'No se encuentra la comisaria']],404);
+		}
+
+		$array=VotoComisaria::where('ComCod','=',$id)->get();
+		if(!count($array))
+		{
+			return response()->json(['mensaje'=>'Esta comisaria no ha recibido votos aun.'],200);
+		}
+
+		$sum = 0;
+		$cont = 0;
+		foreach ($array as $voto) 
+		{
+			$sum += $voto->VotPun;
+			$cont++;
+		}
+		$prom = $sum/$cont;
+		return response()->json(['response'=>['promedio'=>$prom]],200);
+	}
 }
